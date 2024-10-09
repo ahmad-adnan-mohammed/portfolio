21db76f40a05366faa1bf37c265fbe92f1ce3d20
ahmad-adnan-mohammed <58122601+ahmad-adnan-mohammed@users.noreply.github.com>
2024-10-09 17:29:37 +0100

    Delete ahmad directory

--- ahmad/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.toggle-details').on('click', function() {
-        $(this).siblings('.project-details').slideToggle();
-        $(this).text($(this).text() === 'View Details' ? 'Hide Details' : 'View Details');
-    });
-});
