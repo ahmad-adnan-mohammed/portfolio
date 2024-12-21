19ec78fc01d5c74a71abb125bc42233eb383a30b
ahmad-adnan-mohammed <58122601+ahmad-adnan-mohammed@users.noreply.github.com>
2024-12-21 22:26:25 +0100

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+$(document).ready(function() {
+    $('.toggle-details').on('click', function() {
+        $(this).siblings('.project-details').slideToggle();
+        $(this).text($(this).text() === 'View Details' ? 'Hide Details' : 'View Details');
+    });
+
+    // Function to handle flipping
+    function toggleDetails(element) {
+        const $details = $(element).siblings('.project-details');
+        $details.slideToggle();
+        const $toggle = $(element);
+        $toggle.text($toggle.text() === 'View Details' ? 'Hide Details' : 'View Details');
+    }
+
+    // Event listener for 'click' and 'touchstart'
+    $('.toggle-details').on('click touchstart', function(event) {
+        event.preventDefault(); // Prevent default touch behavior
+        toggleDetails(this);
+    });
+});
+
+
+
+
 $(document).ready(function () {
     $('.flip-card').on('click touchend', function (event) {
         event.preventDefault(); // Prevent any default behavior
